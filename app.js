e3c8c7deb3b7ddc00985ea5504b03ffcc92da2e6
Fkremaryam (jo) <154042886+Lioglue@users.noreply.github.com>
2026-05-09 20:19:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,27 @@ const supabase =
         supabaseKey
     );
 
+/* MOBILE MENU */
+
+function toggleMenu() {
+
+    const nav =
+        document.getElementById("navLinks");
+
+    nav.classList.toggle("active");
+
+}const supabaseUrl =
+    "https://fmlitydfcydcnnumhmyo.supabase.co";
+
+const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtbGl0eWRmY3lkY25udW1obXlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1NjQ0OTAsImV4cCI6MjA5MzE0MDQ5MH0.lMevNLxXr9w3uPY4q_wbkHhYqY-ogERRrzoRc-qVWWU";
+
+const supabase =
+    window.supabase.createClient(
+        supabaseUrl,
+        supabaseKey
+    );
+
 function toggleMenu(){
 
     const nav =
