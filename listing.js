7a26a360712257f51250a255e6b324550c17b887
Fkremaryam (jo) <154042886+Lioglue@users.noreply.github.com>
2026-05-15 20:28:31 +0300

    Add function to insert listings into Supabase
    Implemented the addListing function to insert new listings into the Supabase database with user input.

--- listing.js
+++ listing.js
@@ -1,1 +1,59 @@
+const supabaseUrl =
+'https://fmlitydfcydcnnumhmyo.supabase.co';
 
+const supabaseKey =
+'sb_publishable_WX8hbbfYCzNZIVqvq5Wsjw_LrABY0eZ';
+
+const supabase = window.supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+);
+
+/* ADD LISTING */
+
+async function addListing(){
+
+    const title =
+        document.getElementById("title").value;
+
+    const description =
+        document.getElementById("description").value;
+
+    const price =
+        document.getElementById("price").value;
+
+    const image =
+        document.getElementById("image").value;
+
+    const type =
+        document.getElementById("type").value;
+
+    /* INSERT INTO DATABASE */
+
+    const { data, error } =
+        await supabase
+        .from("listings")
+        .insert([
+            {
+                title: title,
+                description: description,
+                price: price,
+                image_url: image,
+                type: type
+            }
+        ]);
+
+    if(error){
+
+        alert(error.message);
+
+    }
+    else{
+
+        alert("Listing Published Successfully");
+
+        window.location.href = "index.html";
+
+    }
+
+}
